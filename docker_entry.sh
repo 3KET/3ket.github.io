625ca685d6b788907caca89020075320060d9e62
Ege Kaan Gürkan <egekaanngurkan@gmail.com>
2021-10-10 20:27:49 +0300

    Update docker_entry.sh

--- docker_entry.sh
+++ docker_entry.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# git config --global user.email "${GIT_EMAIL}"
-# git config --global user.name "${GIT_USERNAME}"
+git config --global user.email "${GIT_EMAIL}"
+git config --global user.name "${GIT_USERNAME}"
 
 git config --global credential.helper \
     '!f() { echo "username=$GIT_USERNAME"; echo "password=$GIT_TOKEN"; };f'
